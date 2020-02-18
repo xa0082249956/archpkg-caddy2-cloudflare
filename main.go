4e801e50ce13d27e3d1458081e91eac6eb18e1d2
xa0082249956 <576810415@qq.com>
2020-02-18 14:58:32 +0800

    add standard modules

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ package main
 
 import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/caddyserver/tls.dns/providers/cloudflare"
 )
 
